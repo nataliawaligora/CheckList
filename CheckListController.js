182b64bb7a0b3ace15e10ae69849b280b318d0ef
nataliawaligora <fliberczibit@gmail.com>
2016-08-17 21:32:34 +0200

    Podklad pod checkliste

--- CheckListController.js
+++ CheckListController.js
@@ -1,8 +1,9 @@
 (function () {
     var app = angular.module("checkList", []);
     app.controller('CheckListController', function ($scope) {
+        $scope.chcemGreena = false;
         $scope.items = [
-            {title: 'The list of the product'}
+            {title: 'The list of the product', isDone: false}
 
         ];
 
@@ -10,9 +11,12 @@
             console.log('dodaj itema kurdeee');
             console.log('title nowego itema', $scope.text);
             $scope.items.push({title: $scope.text});
+            $scope.text = '';
+
         };
-        // $scope.hideItem = function(){
-        //     $scope.
-        // }
+        $scope.moveIt = function () {
+            // $scope.list.hide();
+            console.log('aaa!');
+        }
     });
 })();
